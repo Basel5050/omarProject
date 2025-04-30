51e5d900f4f76ec978c52891216d40ad47d3d146
Basel5050 <baselashraf1@gmail.com>
2025-04-30 04:09:09 +0300

    logo2

--- src/pages/users/components/UserFooter.jsx
+++ src/pages/users/components/UserFooter.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import { FaWhatsapp, FaLinkedinIn, FaBehance, FaInstagram } from 'react-icons/fa';
-import Logo from './logo/Logo';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const UserFooter = () => {
   return (
     <footer className="bg-black py-8 px-4 flex flex-col items-center justify-center font-outfit text-center">
       
       {/* اللوجو او الاسم */}
-      <Logo/>
-
+      <Link to="/" className="block">
+            <motion.img
+              src="/omarLogo.png"
+              alt="Omar Logo"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.6, ease: "easeOut" }}
+              className="h-32 sm:h-40 md:h-40 p-4  object-contain drop-shadow-[0_4px_10px_rgba(255,255,255,0.25)]"
+            />
+          </Link>
       {/* Social Media Links */}
       <div className="flex gap-6 mb-6">
         <a href="https://wa.me/201113503504" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition">
